399527c7b91367bcfb85169ca40a732eac5eb777
Jochen Reinholdt <jochen.reinholdt@syde-consulting.ch>
2021-05-10 16:55:04 +0200

    Add existing file

--- HFSNT.H.2.11B-BE/mkstruct1.sh
+++
@@ -1,7 +0,0 @@
-# mkstruct1.sh legt eine feste Vereichnisstruktur mit dem mkdir-Kommando an
-# mit Auswertung einer vorher gesetzten Variablen
-# wenn sie nicht existiert oder leer ist, soll der Default-Name "Kurs" sein
-Wurzel=${KURSNAME:-Kurs}
-mkdir -p ${Wurzel}/block1/aufgaben
-mkdir -p ${Wurzel}/block1/loesungen
-# usw ...
